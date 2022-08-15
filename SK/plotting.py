Minor re-write with fixed error level calculation. Number of uncertainty sigma levels can now be specified with --nsig Errors on RA and Dec now printed to terminal Likelihood can be written to fits file with --fits Plotting gussied up Overall more pythonic

SK/plotting.py
@@ -1,132 +1,132 @@
 #!/usr/bin/env python
-#Tiaan Bezuidenhout, 2020. For inquiries: bezmc93@gmail.com
-#NB: REQUIRES Python 3
+# Tiaan Bezuidenhout, 2020. For inquiries: bezmc93@gmail.com
+# NB: REQUIRES Python 3
 
-'''
+"""
 Plotting tools.
-'''
+"""
 
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy.coordinates import SkyCoord
-import astropy.units  as u
+import astropy.units as u
 from mpl_toolkits.axes_grid1.anchored_artists import AnchoredSizeBar
+import matplotlib.font_manager as fm
 
 import SK.utils as ut
 import SK.coordinates as co
 
 
 def plot_known(w,known_coords):
-	'''
-	Makes a cross on the localisation plot where a known source is located.
-	w must be a WCS object pre-set with SK_coordinates.buildWCS
-	'''
+    """
+    Makes a cross on the localisation plot where a known source is located.
+    w must be a WCS object pre-set with SK_coordinates.buildWCS
+    """
+    known_coords = known_coords.split(',')
 
-	known_coords = known_coords.split(',')
+    try:
+        known_coords = [float(known_coords[0]), float(known_coords[1])]
+    except:
+        known_coords = SkyCoord(known_coords[0], known_coords[1], frame='icrs', 
+                                unit=(u.hourangle, u.deg))
+        known_coords = [known_coords.ra.deg, known_coords.dec.deg]
 
-	try:
-		known_coords = [float(known_coords[0]),float(known_coords[1])]
-	except:
-		known_coords = SkyCoord(known_coords[0],known_coords[1], frame='icrs', unit=(u.hourangle, u.deg))
-		known_coords = [known_coords.ra.deg,known_coords.dec.deg]
+    known_px = w.all_world2pix([known_coords], 1)
+
+    plt.scatter(known_px[0, 0],known_px[0, 1], c='cyan', marker='x', s=20, zorder=999)
 
 
-	known_px = w.all_world2pix([known_coords],1)
-	
-	plt.scatter(known_px[0,0],known_px[0,1],c='cyan',marker='x',s=20,zorder=999)
+def make_ticks(ax, array_width, array_height, w, fineness):
+    """
+    Adds ticks and labels in sky coordinates to the plot.
+    """
 
-def make_ticks(ax,array_width,array_height,w,fineness):
-	'''
-	Adds ticks and labels in sky coordinates to the plot.
-	'''
-	
-	ticks = ut.getTicks(array_width,array_height,w,fineness)
-	labels = co.pix2deg(ticks,w)
-	ra_deg= np.around(labels[:,0],4)
-	dec_deg = np.around(labels[:,1],4)
-	ax.set_xticks(ticks[0])#,rotation=40,fontsize=8)
-	ax.set_xticklabels(ra_deg,rotation=40)
-	ax.set_yticks(ticks[1])#, dec_deg)#,fontsize=8)
-	ax.set_yticklabels(dec_deg)
-	
+    ticks = ut.getTicks(array_width, array_height, w, fineness)
+    labels = co.pix2deg(ticks, w)
+    ra_deg= np.around(labels[:, 0], 4)
+    dec_deg = np.around(labels[:, 1], 4)
+    ax.set_xticks(ticks[0])
+    ax.set_xticklabels(ra_deg, rotation=40, fontsize=12)
+    ax.set_yticks(ticks[1])
+    ax.set_yticklabels(dec_deg, fontsize=12)
 
-def likelihoodPlot(f,ax,loglikelihood,options):
-    '''
+
+def likelihoodPlot(f, ax, w, loglikelihood, options):
+    """
     Creates the localisation plot
-    '''
-    #np.save('CB_localisation.npy',loglikelihood)
+    """
+    
+    likelihood = ut.norm_likelihood(loglikelihood)
 
-    likelihood = np.exp(loglikelihood - np.nanmax(loglikelihood))
-    likelihood = np.nan_to_num(likelihood)
+    import matplotlib.colors
+    cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["#FFFFFF","#1E88E5"])
 
-    likelihood /= likelihood.sum()
+    plt.imshow(likelihood, origin='lower', cmap=cmap, aspect="auto")
+    plt.subplots_adjust(left=0.12, right=0.85, bottom=0.1, top=0.85)
 
-    likelihood[likelihood<0.000000001] = 0.0
-    plt.imshow(likelihood,origin='lower',cmap='binary',aspect="auto")
-    plt.subplots_adjust(left=0.1,right=0.85,bottom=0.1,top=0.85)
+    plt.scatter(np.where(likelihood==np.amax(likelihood))[1],
+                np.where(likelihood==np.amax(likelihood))[0], 
+                marker='x', c='red')
 
-    ## Making adjustable scale bar    
+    ## Making adjustable scale bar 
+    fontprops = fm.FontProperties(size=14)   
     if options.sb[0] != 0:
         scalebar = AnchoredSizeBar(ax.transData,
-                           int(options.sb[0]/options.res[0]), '%d arcsec' % (options.sb[0]), 'upper left', 
+                           int(options.sb[0]/options.res[0]), 
+                           '%d arcsec' % (options.sb[0]), 
+                           'upper left', 
                            pad=1.0,
-                           color='black',
+                           color='#1d3557',
                            frameon=True,
-                           size_vertical=0.2)
+                           size_vertical=0.2,
+                           fontproperties=fontprops)
 
         ax.add_artist(scalebar)
-
-    #cbar = plt.colorbar()
-
-    plt.scatter(np.where(likelihood==np.amax(likelihood))[1],np.where(likelihood==np.amax(likelihood))[0],marker='x',c='red')
+    
+    # Printing location of maximum likelihood
+    max_loc = np.where(loglikelihood==np.nanmax(loglikelihood))
+    ut.printCoords(max_loc, w)
+    if len(max_loc) == 2:
+        plt.axhline(max_loc[0], lw=1.2, c='#a8dadc', ls='--')
+        plt.axvline(max_loc[1], lw=1.2, c='#a8dadc', ls='--')
 
     ## Calculating the interval values in 2D
-    likelihood_flat_sorted = np.sort(likelihood, axis=None)
-    likelihood_flat_sorted_cumsum = np.cumsum(likelihood_flat_sorted)
+    print('Error levels:')
+    sigs = np.asarray(np.arange(1,options.nsig[0]+1))
 
-    if len(np.nonzero(likelihood_flat_sorted_cumsum > (1-0.6827))[0]) != 0:
-        ind_1sigma = np.nonzero(likelihood_flat_sorted_cumsum > (1-0.6827))[0][0]
-        val_1sigma = likelihood_flat_sorted[ind_1sigma]
-        CS1 = plt.contour(likelihood,levels=[val_1sigma],zorder=800,colors='red')
-    else:
-        val_1sigma = np.nan
+    for s in sigs:
+        level, error = ut.calc_error(likelihood, s)
 
-    if len(np.nonzero(likelihood_flat_sorted_cumsum > (1-0.9545))[0]) != 0:
-        ind_2sigma = np.nonzero(likelihood_flat_sorted_cumsum > (1-0.9545))[0][0]
-        val_2sigma = likelihood_flat_sorted[ind_2sigma]
-        CS2 = plt.contour(likelihood,levels=[val_2sigma],zorder=800,linestyles='--',colors='red')
-    else:
-        val_2sigma = np.nan
+        if s == 1:
+            ls = '-'
+            lc = '#e63946'
+            lw = 3
 
-    if len(np.nonzero(likelihood_flat_sorted_cumsum > (1-0.9973))[0]) != 0:
-        ind_3sigma = np.nonzero(likelihood_flat_sorted_cumsum > (1-0.9973))[0][0]
-        val_3sigma = likelihood_flat_sorted[ind_3sigma]
-        #CS3 = plt.contour(likelihood,levels=[val_3sigma],zorder=800,colors='lightgray')
-    else:
-        val_3sigma = np.nan
+        else:
+            ls = '--'
+            lc = '#E65476'
+            lw = 2
 
-    lines = [ CS1.collections[0], CS2.collections[0]]
-    labels = ['1-$\sigma$','2-$\sigma$']
-    plt.legend(lines, labels, loc='upper right', borderpad=1.0, edgecolor='black')
+        plt.contour(likelihood, levels=[level], zorder=800,
+                        colors=lc, linestyles=ls, linewidths=lw)
+
+        print('---- %i sigma ----' % (s))
+        ut.printError(max_loc, w, error, s)
 
     ## Making axis histograms
-    ax_histx = f.add_axes([0.1,0.855,0.75,0.1], sharex=ax)
-    ax_histx.plot(np.sum(likelihood,axis=0),color='black')
+    ax_histx = f.add_axes([0.12, 0.855, 0.73, 0.1], sharex=ax)
+    ax_histx.plot(np.sum(likelihood, axis=0),color='black')
     plt.setp(ax_histx.get_xticklabels(), visible=False)
-
-    # ax_histx.fill_between(x=range(0,likelihood.shape[1]),y1=-0.1*max(np.sum(likelihood,axis=0)),y2=np.sum(likelihood,axis=0),where=np.amax(likelihood,axis=0)>=val_3sigma,color='magenta')
-    # ax_histx.fill_between(x=range(0,likelihood.shape[1]),y1=-0.1*max(np.sum(likelihood,axis=0)),y2=np.sum(likelihood,axis=0),where=np.amax(likelihood,axis=0)>=val_2sigma,color='lime')
-    # ax_histx.fill_between(x=range(0,likelihood.shape[1]),y1=-0.1*max(np.sum(likelihood,axis=0)),y2=np.sum(likelihood,axis=0),where=np.amax(likelihood,axis=0)>=val_1sigma, color='cyan')
-
-    ax_histy = f.add_axes([0.855,0.1,0.1,0.75], sharey=ax)
-    ax_histy.plot(np.sum(likelihood,axis=1),range(0,likelihood.shape[0]),color='black')
-    ax_histy.set_title('Likelihood',size=10)
+    ax_histx.vlines(max_loc[1], 0, np.max(np.sum(likelihood, axis=0)) ,
+                     lw=1.2, colors='#a8dadc', ls='--')
+    
+    ax_histy = f.add_axes([0.855, 0.1, 0.1, 0.75], sharey=ax)
+    ax_histy.plot(np.sum(likelihood, axis=1),np.arange(0,likelihood.shape[0]), 
+                    color='black')
+    ax_histy.set_title('Likelihood', size=10)
     plt.setp(ax_histy.get_yticklabels(), visible=False)
+    ax_histy.hlines(max_loc[0], 0, np.max(np.sum(likelihood, axis=1)) ,
+                     lw=1.2, colors='#a8dadc', ls='--')
 
-    # ax_histy.fill_betweenx(y=range(0,likelihood.shape[0]),x1=-0.1*max(np.sum(likelihood,axis=1)),x2=np.sum(likelihood,axis=1),where=np.amax(likelihood,axis=1)>=val_3sigma, color='magenta')
-    # ax_histy.fill_betweenx(y=range(0,likelihood.shape[0]),x1=-0.1*max(np.sum(likelihood,axis=1)),x2=np.sum(likelihood,axis=1),where=np.amax(likelihood,axis=1)>=val_2sigma, color='lime')
-    #ax_histy.fill_betweenx(y=range(0,likelihood.shape[0]),x1=-0.1*max(np.sum(likelihood,axis=1)),x2=np.sum(likelihood,axis=1),where=np.amax(likelihood,axis=1)>=val_1sigma, color='cyan')
-
-
-    ax.set_xlabel('RA ($^\circ$)')
-    ax.set_ylabel('Dec ($^\circ$)')
+    ax.set_xlabel("RA ($^\circ$)", fontsize=15)
+    ax.set_ylabel("Dec ($^\circ$)", fontsize=15)

SK/utils.py
@@ -1,236 +1,340 @@
 #!/usr/bin/env python
-#Tiaan Bezuidenhout, 2020. For inquiries: bezmc93@gmail.com
-#NB: REQUIRES Python 3
+# Tiaan Bezuidenhout, 2020. For inquiries: bezmc93@gmail.com
+# NB: REQUIRES Python 3
 
-'''
+"""
 Tools for read reading in CB location files, PSF fits files, etc.
-'''
+"""
 
 import json
 import numpy as np
 import math
 from astropy.coordinates import SkyCoord
-import astropy.units  as u
+import astropy.units as u
 from astropy.io import fits
+from scipy.interpolate import griddata
 
 import SK.coordinates as co
 
+
 def readCoords(options):
-	'''
-	Checks that arguments make sense, and if so returns data read from input file. 
-	'''
-	data = np.genfromtxt(options.file[0],
-			delimiter=' ',
-			dtype=None,
-			names=["RA","Dec","SN"],
-			encoding="ascii")
+    """
+    Checks that arguments make sense, and if so returns data read from input file. 
+    """
 
-	data = data[np.argsort(data["SN"])[::-1]] #sorting beams by S/N, so that Gaussian assumption holds better. You generally want LOW/HIGH S/N beam pairs.
+    data = np.genfromtxt(options.file[0],
+            delimiter=' ',
+            dtype=None,
+            names=["RA", "Dec", "SN"],
+            encoding="ascii")
+
+    # Sorting beams by S/N, so that Gaussian assumption holds better. 
+    # You generally want LOW/HIGH S/N beam pairs.
+    data = data[np.argsort(data["SN"])[::-1]] 
+
+    c = SkyCoord(data["RA"],data["Dec"], frame="icrs",unit=(u.hourangle, u.deg))
+    best_cand = np.argsort(data["SN"])[-1]
+
+    # Calculate number of beam pairs
+    n_comb = math.factorial(len(data))/(math.factorial(2)*
+    						math.factorial(len(data)-2)) 
+
+    if not options.config:
+        if options.source:
+            [ra,dec] = options.source[0].split(',')
+            b = SkyCoord(ra, dec, frame='icrs',unit=(u.hourangle, u.deg))
+            boresightCoord = [b.ra.deg,b.dec.deg]
+        else:
+            bs_c = SkyCoord(data["RA"][best_cand],data["Dec"][best_cand], frame='icrs', unit=(u.hourangle, u.deg))
+            boresightCoord = [bs_c.ra.deg,bs_c.dec.deg]
+
+        if options.overlap > 1.0 or options.overlap < 0:
+            print("The OVERLAP parameter must be between 0 and 1")
+            exit()
+
+        elif options.npairs[0] > n_comb:
+            options.npairs[0] = n_comb
+            return data, c, boresightCoord
+        else:
+            return data, c, boresightCoord
+    else:
+        ra, dec, options.overlap = readJSON(options.config[0])
+        b = SkyCoord(ra, dec, frame='icrs',unit=(u.hourangle, u.deg))
+        boresightCoord = [b.ra.deg, b.dec.deg]
+
+        if options.npairs[0] > n_comb:
+            options.npairs[0] = n_comb
+            return data, c, boresightCoord
+        else:
+            return data, c, boresightCoord
 
 
-	c = SkyCoord(data["RA"],data["Dec"], frame="icrs",unit=(u.hourangle,u.deg))
-	best_cand = np.argsort(data["SN"])[-1]
-		
-	n_comb = math.factorial(len(data))/(math.factorial(2)*math.factorial(len(data)-2)) #number of beam pairs
-
-	if not options.config:
-		if options.source:
-			[ra,dec] = options.source[0].split(',')
-			b = SkyCoord(ra, dec, frame='icrs',unit=(u.hourangle, u.deg))
-			boresightCoord = [b.ra.deg,b.dec.deg]	
-		else:
-			bs_c = SkyCoord(data["RA"][best_cand],data["Dec"][best_cand], frame='icrs', unit=(u.hourangle, u.deg))
-			boresightCoord = [bs_c.ra.deg,bs_c.dec.deg]
-
-		if options.overlap > 1.0 or options.overlap < 0:
-			print("The OVERLAP parameter must be between 0 and 1")
-			exit()
-
-		elif options.npairs[0] > n_comb:
-			options.npairs[0] = n_comb
-			return data,c,boresightCoord
-		else:
-			return data,c,boresightCoord
-	else:
-		ra, dec, options.overlap = readJSON(options.config[0])
-		b = SkyCoord(ra, dec, frame='icrs',unit=(u.hourangle, u.deg))
-		boresightCoord = [b.ra.deg,b.dec.deg]
-
-		if options.npairs[0] > n_comb:
-			options.npairs[0] = n_comb
-			return data,c,boresightCoord
-
-	return data,c,boresightCoord
-	
 def readPSF(psf,clip):
-	'''
-	Converts PSF in fits format to a numpy array
-	'''
-	hdul = fits.open(psf)
-	psf_ar = hdul[0].data
+    """
+    Converts PSF in fits format to a numpy array
+    """
 
-	# CLIPPING
-	# Note: clipping the psf below a certain sensitivity makes for a cleaner
-	#		final localisation by mitigating the effect of low-level sidelobes.
-	#		I find 8% is a good cutoff for detections in multiple adjacent beams,
-	#		but for detections further away from any CB boresight you might want
-	#		to reduce that. 
+    hdul = fits.open(psf)
+    psf_ar = hdul[0].data
 
-	psf_ar[psf_ar<clip] = 0
+    # CLIPPING
+    # Note: clipping the psf below a certain sensitivity makes for a cleaner
+    #       final localisation by mitigating the effect of low-level sidelobes.
+    #       I find 8% is a good cutoff for detections in multiple adjacent beams,
+    #       but for detections further away from any CB boresight you might want
+    #       to reduce that. 
 
-	return psf_ar
+    psf_ar[psf_ar < clip] = 0.0
 
-def write2fits(w,likelihood):
+    return psf_ar
+
+
+def norm_likelihood(loglikelihood):
+    likelihood = np.exp(loglikelihood - np.nanmax(loglikelihood))
+    likelihood = np.nan_to_num(likelihood)
+    likelihood /= likelihood.sum()
+    likelihood[likelihood<0.000000001] = 0.0
+
+    return likelihood
+
+
+def CDF(s):
+    # Cumulative distribution function for 2D Gaussian
+    return  1 - np.exp(-0.5 * s ** 2)
+
+
+def calc_error(likelihood, sigma):
+    likelihood_flat_sorted = np.sort(likelihood, axis=None)
+    likelihood_flat_sorted_index = np.argsort(likelihood, axis=None)
+    likelihood_flat_sorted_cumsum = np.cumsum(likelihood.flatten()[likelihood_flat_sorted_index])
+
+    if len(np.nonzero(likelihood_flat_sorted_cumsum > (1 - CDF(sigma)))[0]) != 0:
+        # Index where cum sum goes above sigma percentage
+        index_sigma_above = np.nonzero(likelihood_flat_sorted_cumsum > 
+                        (1 - CDF(sigma)))[0]
+
+        # Minimum likelihood included in error
+        likelihood_index_sigma_sorted = likelihood_flat_sorted_index[index_sigma_above]
+
+        likelihood_sigma_level = likelihood_flat_sorted[index_sigma_above[0]]
+
+        likelihood_index_sigma_original = np.unravel_index(likelihood_index_sigma_sorted, 
+                                                            likelihood.shape)
+        y_lower_index, y_higher_index = np.sort(likelihood_index_sigma_original[0])[[np.s_[0], 
+                                                np.s_[-1]]]
+        x_lower_index, x_higher_index = np.sort(likelihood_index_sigma_original[1])[[np.s_[0], 
+                                                np.s_[-1]]]
+        max_likehood_index = np.unravel_index(np.argmax(likelihood), likelihood.shape)
+
+        x_lower = x_lower_index, max_likehood_index[0]
+        x_higher = x_higher_index, max_likehood_index[0]
+        y_lower = max_likehood_index[1], y_lower_index
+        y_higher = max_likehood_index[1], y_higher_index
+
+        return likelihood_sigma_level, [x_lower, x_higher, y_lower, y_higher]
+
+    else:
+        print('Could not find error')
+        exit()
+
+
+def to_HMS_second(coord):
+        return coord.hms.h*60*60 + coord.hms.m*60 + coord.hms.s
+
+def to_DMS_second(coord):
+        return coord.dms.d*60*60 + coord.dms.m*60*60 + coord.dms.s
+
+
+def printCoords(max_loc, w):
+    if len(max_loc) == 2:
+        max_loc = (max_loc[1],max_loc[0])
+        max_deg = []
+        max_deg.append(co.pix2deg(max_loc, w)[0])
+        max = SkyCoord(max_deg, unit='deg')
+        print('\nMaximum likelihood at coordinates:')
+        print('{:.3f}, {:.3f} deg'.format(max.ra.deg[0], max.dec.deg[0]))
+
+        print(max.ra.to_string(u.hour)[0] + ', ' + 
+                max.dec.to_string(u.degree, alwayssign=True)[0])
+        print('')
+
+
+def printError(max_loc, w, error, sigma):
+    if len(max_loc) == 2:
+        max_loc = (max_loc[1],max_loc[0])
+
+        max_deg = []
+        max_deg.append(co.pix2deg(max_loc, w)[0])
+        center_eq = SkyCoord(max_deg, unit='deg')
+
+        error_eq_deg =  w.all_pix2world(error, 0)
+        error_eq = SkyCoord(error_eq_deg, unit='deg')
+
+        ra_lower = center_eq.ra - error_eq[0].ra
+        ra_higher = error_eq[1].ra - center_eq.ra
+        dec_lower = center_eq.dec - error_eq[2].dec
+        dec_higher = error_eq[3].dec - center_eq.dec
+    
+        print("| RA error (s): -{:.3f}, +{:.3f}".format(
+                np.abs(to_HMS_second(ra_lower)[0]),
+                np.abs(to_HMS_second(ra_higher)[0])))
+        print("| DEC error (arcs): -{:.3f}, +{:.3f}".format(
+                np.abs(to_DMS_second(dec_lower)[0]),
+                np.abs(to_DMS_second(dec_higher)[0])))
+        print('')
+
+
+def write2fits(w, likelihood):
     header = w.to_header()
     hdu = fits.PrimaryHDU(header=header)
     hdu.data = likelihood
     hdu.update_header
-    hdu.writeto('localisation.fits')
+    fitsname = 'Likelihood.fits'
+    hdu.writeto(fitsname, overwrite=True)
+
 
 def readJSON(config):
-	'''
-	Read tiling configuration from JSON file
-	Note: 	this is very specific for the MeerTRAP pipeline's JSON log files,
-			needs generalisation.  
-	'''
+    """
+    Read tiling configuration from JSON file
+    Note: this is very specific for the MeerTRAP pipeline's JSON log files,
+          needs generalisation.  
+    """
 
-	with open(config) as json_file:
-		data = json.load(json_file)	
-	ra = data['beams']['ca_target_request']['tilings'][0]['target'].split(',')[2]
-	dec = data['beams']['ca_target_request']['tilings'][0]['target'].split(',')[3]
-	overlap = str(data['beams']['ca_target_request']['tilings'][0]['overlap'])
+    with open(config) as json_file:
+        data = json.load(json_file)	
+    ra = data['beams']['ca_target_request']['tilings'][0]['target'].split(',')[2]
+    dec = data['beams']['ca_target_request']['tilings'][0]['target'].split(',')[3]
+    overlap = str(data['beams']['ca_target_request']['tilings'][0]['overlap'])
 
-	return ra, dec, overlap
+    return ra, dec, overlap
 
 def get_best_pairs(data,npairs):
-	'''
-	If we want to do the localisation using only the N highest-S/N beam pairs
-	for the sake of time, this determines which ones to use.
-	'''
-	sums = []
-	for i in range (0,len(data["SN"])):
-		for j in range (0,len(data["SN"])):
-			if i !=j:
-				sums.append(data["SN"][i]+data["SN"][j])
+    """
+    If we want to do the localisation using only the N highest-S/N beam pairs
+    for the sake of time, this determines which ones to use.
+    """
 
-	threshold = sorted(sums)[-2*npairs]
+    sums = []
+    for i in range (0,len(data["SN"])):
+        for j in range (0,len(data["SN"])):
+            if i !=j:
+                sums.append(data["SN"][i] + data["SN"][j])
 
-	return threshold
+    threshold = sorted(sums)[-2*npairs]
+
+    return threshold
+
 
 def getTicks(array_width,array_height,w,fineness):
-	'''
-	Returns tick labels for a given size of numpy array.
-	'''
-	try:
-		num_ticks = min(array_width,array_height)/fineness
-		ticks = [np.arange(0,min(array_width,array_height),min(array_width,array_height)/num_ticks), np.arange(0,min(array_width,array_height),min(array_width,array_height)/num_ticks)]
+    """
+    Returns tick labels for a given size of numpy array.
+    """
 
-	except:
-		num_ticks = 10
-		ticks = [np.arange(0,min(array_width,array_height),min(array_width,array_height)/num_ticks), np.arange(0,min(array_width,array_height),min(array_width,array_height)/num_ticks)]
-	
-	return ticks
+    try:
+        num_ticks = min(array_width,array_height)/fineness
+        ticks = [np.arange(0,min(array_width,array_height),min(array_width,array_height)/num_ticks), np.arange(0,min(array_width,array_height),min(array_width,array_height)/num_ticks)]
+
+    except:
+        num_ticks = 10
+        ticks = [np.arange(0,min(array_width,array_height),min(array_width,array_height)/num_ticks), np.arange(0,min(array_width,array_height),min(array_width,array_height)/num_ticks)]
+
+    return ticks
 
 
 def readSubbandingFiles(options):
-	dataLocs = np.genfromtxt(options.locFile[0],
-			delimiter=' ',
-			dtype=None,
-			names=["RA","Dec","fil","SNR"],
-			encoding="ascii")
+    dataLocs = np.genfromtxt(options.locFile[0],
+            delimiter=' ',
+            dtype=None,
+            names=["RA", "Dec", "fil", "SNR"],
+            encoding="ascii")
 
-	dataSNR = np.genfromtxt(options.snrFile[0],
-			delimiter=' ',
-			dtype=None,
-			names=["fil","SNR"],
-			encoding="ascii")
+    dataSNR = np.genfromtxt(options.snrFile[0],
+            delimiter=' ',
+            dtype=None,
+            names=["fil", "SNR"],
+            encoding="ascii")
 
-	c = SkyCoord(dataLocs["RA"],dataLocs["Dec"], frame="icrs",unit=(u.hourangle,u.deg))
-	best_cand = np.argsort(dataLocs["SNR"])[-1]
-	n_comb = math.factorial(len(dataLocs))/(math.factorial(2)*math.factorial(len(dataLocs)-2)) #number of beam pairs
+    c = SkyCoord(dataLocs["RA"],dataLocs["Dec"], frame="icrs",unit=(u.hourangle,u.deg))
+    best_cand = np.argsort(dataLocs["SNR"])[-1]
+    n_comb = math.factorial(len(dataLocs)) / (math.factorial(2)*
+            math.factorial(len(dataLocs) - 2)) #number of beam pairs
 
-	if not options.config:
-		if options.source:
-			[ra,dec] = options.source[0].split(',')
-			b = SkyCoord(ra, dec, frame='icrs',unit=(u.hourangle, u.deg))
-			boresightCoord = [b.ra.deg,b.dec.deg]	
-		else:
-			bs_c = SkyCoord(dataLocs["RA"][best_cand],dataLocs["Dec"][best_cand], frame='icrs', unit=(u.hourangle, u.deg))
-			boresightCoord = [bs_c.ra.deg,bs_c.dec.deg]
+    if not options.config:
+        if options.source:
+            [ra,dec] = options.source[0].split(',')
+            b = SkyCoord(ra, dec, frame='icrs',unit=(u.hourangle, u.deg))
+            boresightCoord = [b.ra.deg, b.dec.deg]	
+        else:
+            bs_c = SkyCoord(dataLocs["RA"][best_cand],
+                            dataLocs["Dec"][best_cand], 
+                            frame='icrs', unit=(u.hourangle, u.deg))
+            boresightCoord = [bs_c.ra.deg,bs_c.dec.deg]
 
-		if options.overlap > 1.0 or options.overlap < 0:
-			print("The OVERLAP parameter must be between 0 and 1")
-			exit()
+        if options.overlap > 1.0 or options.overlap < 0:
+            print("The OVERLAP parameter must be between 0 and 1")
+            exit()
 
-		elif options.npairs[0] > n_comb:
-			options.npairs[0] = n_comb
-			return dataLocs,dataSNR,c,boresightCoord
-		else:
-			return dataLocs,dataSNR,c,boresightCoord
-	else:
-		ra, dec, options.overlap = readJSON(options.config[0])
-		b = SkyCoord(ra, dec, frame='icrs',unit=(u.hourangle, u.deg))
-		boresightCoord = [b.ra.deg,b.dec.deg]
+        elif options.npairs[0] > n_comb:
+            options.npairs[0] = n_comb
+            return dataLocs,dataSNR,c,boresightCoord
+        else:
+            return dataLocs,dataSNR,c,boresightCoord
+    else:
+        ra, dec, options.overlap = readJSON(options.config[0])
+        b = SkyCoord(ra, dec, frame='icrs',unit=(u.hourangle, u.deg))
+        boresightCoord = [b.ra.deg,b.dec.deg]
 
-		if options.npairs[0] > n_comb:
-			options.npairs[0] = n_comb
-			return dataLocs,dataSNR,c,boresightCoord
-		
-	return dataLocs,dataSNR,c,boresightCoord
+        if options.npairs[0] > n_comb:
+            options.npairs[0] = n_comb
+            return dataLocs,dataSNR,c,boresightCoord
+        else:
+            return data, c, boresightCoord
+
+    return dataLocs,dataSNR,c,boresightCoord
+
 
 def makeSubbandingPSFcube(options):
-	psf0 = readPSF(options.psf[0],options.clipping[0])
-	psfCube = np.zeros((psf0.shape[0],psf0.shape[1],len(options.psf)))
-	for i in range(0,len(options.psf)):
-		psfCube[:,:,i] = readPSF(options.psf[i],options.clipping[0])
-		
-	return psfCube
+    psf0 = readPSF(options.psf[0], options.clipping[0])
+    psfCube = np.zeros((psf0.shape[0], psf0.shape[1], len(options.psf)))
+    for i in np.arange(0, len(options.psf)):
+        psfCube[:,:,i] = readPSF(options.psf[i], options.clipping[0])
 
-def printCoords(max_loc,w):
-    max_deg = []
-    max_deg.append(co.pix2deg(max_loc,w)[0])
-    max = SkyCoord(max_deg,unit='deg')
-    print('\nMaximum likelihood at coordinates:')
-    print(max.ra.deg[0],max.dec.deg[0])
-    print(max.ra.to_string(u.hour)[0],max.dec.to_string(u.degree,alwayssign=True)[0])
-
-    #text_file = open("locCoords.txt", "a")
-    #text_file.write(str(max.ra.deg[0]) + ' ' + str(max.dec.deg[0]) + '\n')
-    #text_file.close()
+    return psfCube
 
 
-def stretch_IB(ib_psf,res):
-    from scipy.interpolate import griddata
+def autozoom(ax, c, options):
+    ax.set_xlim(min(c.ra.px) - int(15/options.res[0]), 
+                max(c.ra.px) + int(15/options.res[0]))
+    ax.set_ylim(min(c.dec.px) - int(15/options.res[0]), 
+                max(c.dec.px) + int(15/options.res[0]))
 
+
+def stretch_IB(ib_psf, res):
     ib_size = 10 #deg x 10 deg
 
     # cut the IB psf down by this factor to remove some of the padding:
     cutfactor = 4.0 
-    cut = ib_psf.shape[0]/(2*cutfactor)
-    cut1 = int(ib_psf.shape[0]/2.0 - cut)
-    cut2 = int(ib_psf.shape[0]/2.0 + cut)
-    ib_psf = ib_psf[cut1:cut2,cut1:cut2]
-    ib_size = int(ib_size/cutfactor)
+    cut = ib_psf.shape[0] / (2 * cutfactor)
+    cut1 = int(ib_psf.shape[0] / 2.0 - cut)
+    cut2 = int(ib_psf.shape[0] / 2.0 + cut)
+    ib_psf = ib_psf[cut1:cut2, cut1:cut2]
+    ib_size = int(ib_size / cutfactor)
 
-    res_deg = res/3600
+    res_deg = res / 3600.0
     ib_size_px = int(ib_size/res_deg)
-    #print(ib_size_px) # this is the size of the IB in pixels given the resolution of the CB
 
-
-    points = np.zeros((ib_psf.shape[0]*ib_psf.shape[1],2))
+    points = np.zeros((ib_psf.shape[0]*ib_psf.shape[1], 2))
     values = []
-    n=0
+    n = 0
 
-    for i in range (0,ib_psf.shape[0]):
-        for j in range(0,ib_psf.shape[1]):
+    for i in range (0, ib_psf.shape[0]):
+        for j in np.arange(0, ib_psf.shape[1]):
             points[n] = [i, j]
-            values.append(ib_psf[i,j])
-            n+=1
+            values.append(ib_psf[i, j])
+            n += 1
 
     extent = float(ib_psf.shape[0] - 1)
-    grid_x,grid_y = np.mgrid[0:extent:extent/ib_size_px,0:extent:extent/ib_size_px]
-    model_prim = griddata(points,values,(grid_x,grid_y), method='cubic')
+    grid_x,grid_y = np.mgrid[0:extent:extent / ib_size_px, 0:extent:extent / ib_size_px]
+    model_prim = griddata(points, values, (grid_x, grid_y), method='cubic')
 
-    return model_prim,ib_size_px
-
-
+    return model_prim, ib_size_px

SeeKAT_subbanding.py
@@ -20,74 +20,82 @@ import matplotlib.patches as patches
 
 
 def parseOptions(parser):
-	'''Options:
-	-f	Input file
-	--o	Fractional sensitivity level at which CBs are tiled to overlap
-	--r	Resolution of PSF in units of arcseconds per pixel
-	--n	Number of beams to consider when creating overlap contours. Will
-		pick the specified number of beams with the highest S/N values.
-	'''
+    '''Options:
+    -f    Input file
+    --o    Fractional sensitivity level at which CBs are tiled to overlap
+    --r    Resolution of PSF in units of arcseconds per pixel
+    --n    Number of beams to consider when creating overlap contours. Will
+        pick the specified number of beams with the highest S/N values.
+    '''
 
-	parser.add_argument('-l', dest='locFile', 
-				nargs = 1, 
-				type = str, 
-				required=True)
-	parser.add_argument('-c', dest='config', 
-				nargs = 1, 
-				type = str, 
-				help="Configuration (json) file",
-				required=False)
-	parser.add_argument('-s', dest='snrFile', 
-				nargs = 1, 
-				type = str, 
-				required=True)
-	parser.add_argument('-p',dest='psf',
+    parser.add_argument('-l', dest='locFile', 
+                nargs = 1, 
+                type = str, 
+                required=True)
+    parser.add_argument('-c', dest='config', 
+                nargs = 1, 
+                type = str, 
+                help="Configuration (json) file",
+                required=False)
+    parser.add_argument('-s', dest='snrFile', 
+                nargs = 1, 
+                type = str, 
+                required=True)
+    parser.add_argument('-p',dest='psf',
                                 type=str,
                                 nargs = '+', 
                                 help="PSF file",
                                 required=True)
-	parser.add_argument('--o', dest='overlap',
-				type = float,
-				help = "Fractional sensitivity level at which the coherent beams overlap",
-				default = 0.25,
-				required = False)
-	parser.add_argument('--r', dest='res',
-				nargs = 1,
-				type=float,
-				help="Distance in arcseconds represented by one pixel of the PSF",
-				default = 1,
-				required = True)
-	parser.add_argument('--n',dest='npairs',
-				nargs = 1,
-				type = int,
-				default = [1000000])
-	parser.add_argument('--s', dest='source',
-				nargs = 1,
-				type=str,
-				help="Draws given coordinate location (degrees) on localisation plot",
-				required = False)
-	parser.add_argument('--scalebar', dest='sb',
-						nargs = 1,
-						type = float,
-						help = "Length of scale bar on the localisation plot in arcseconds. Set to 0 to omit altogether",
-						default = [10],
-						required = False)
-	parser.add_argument('--ticks', dest='tickspacing',
-						nargs = 1,
-						type = float,
-						help = "Sets the number of pixels between ticks on the localisation plot",
-						default = [50],
-						required = False)
-	parser.add_argument('--clip', dest='clipping',
-						nargs = 1,
-						type = float,
-						help = "Sets values of the PSF below this number to zero. Helps minimise the influence of low-level sidelobes",
-						default = [0.08],
-						required = False)	
+    parser.add_argument('--o', dest='overlap',
+                type = float,
+                help = "Fractional sensitivity level at which the coherent beams overlap",
+                default = 0.25,
+                required = False)
+    parser.add_argument('--r', dest='res',
+                nargs = 1,
+                type=float,
+                help="Distance in arcseconds represented by one pixel of the PSF",
+                default = 1,
+                required = True)
+    parser.add_argument('--n',dest='npairs',
+                nargs = 1,
+                type = int,
+                default = [1000000])
+    parser.add_argument('--nsig',dest='nsig',
+                nargs = 1,
+                type = int,
+                help='Draws uncertainty contours up to this number of standard deviations.',
+                default = [2])
+    parser.add_argument('--s', dest='source',
+                nargs = 1,
+                type=str,
+                help="Draws given coordinate location (degrees) on localisation plot",
+                required = False)
+    parser.add_argument('--scalebar', dest='sb',
+                        nargs = 1,
+                        type = float,
+                        help = "Length of scale bar on the localisation plot in arcseconds. Set to 0 to omit altogether",
+                        default = [10],
+                        required = False)
+    parser.add_argument('--ticks', dest='tickspacing',
+                        nargs = 1,
+                        type = float,
+                        help = "Sets the number of pixels between ticks on the localisation plot",
+                        default = [50],
+                        required = False)
+    parser.add_argument('--clip', dest='clipping',
+                        nargs = 1,
+                        type = float,
+                        help = "Sets values of the PSF below this number to zero. Helps minimise the influence of low-level sidelobes",
+                        default = [0.08],
+                        required = False)    
+    parser.add_argument('--fits', dest='fitsOut',
+                        help = "Outputs .fits file of localisation region",
+                        action = 'store_true')   
 
-	options= parser.parse_args()
+    options= parser.parse_args()
 
-	return options
+    return options
 
 
 
@@ -126,7 +134,7 @@ if __name__ == "__main__":
         fullbandLogLikelihood+=loglikelihood#*np.max(subbandData["SN"])
         #fullbandLogLikelihood /= np.amax(fullbandLikelihood)
   
-    Splot.likelihoodPlot(f,ax,fullbandLogLikelihood,options)
+    Splot.likelihoodPlot(f,ax, w, fullbandLogLikelihood,options)
     max_deg = []
     max_loc = np.where(fullbandLogLikelihood==np.nanmax(fullbandLogLikelihood))
     if len(max_loc) == 2: